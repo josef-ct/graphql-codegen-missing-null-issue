ef4939c35b1864d69235529293e2d770b8354fad
Josef Hrabec <josef.hrabec@ceskatelevize.cz>
2024-03-21 09:03:04 +0100

    Basic next web app

---
+++ next.config.js
@@ -0,0 +1,10 @@
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ **/
+const nextConfig = {
+  distDir: 'dist',
+};
+
+module.exports = nextConfig;
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Homepage = () => {
+  return <p>TEST</p>;
+};
+
+export default Homepage;
